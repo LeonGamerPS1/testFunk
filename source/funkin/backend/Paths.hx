d7cc7374bdfb217cd473dcfb851dbbf89979d905
chungus <chungus@big.com>
2026-02-01 07:14:15 +0100

    paths getGraphicFromCache

--- source/funkin/backend/Paths.hx
+++ source/funkin/backend/Paths.hx
@@ -2,13 +2,22 @@ package funkin.backend;
 
 import flixel.graphics.FlxGraphic;
 
-typedef FlxMapGraphic = Map<String,FlxGraphic>; 
-class Paths {
-    // will get cleared on state switch
-    static var cachedImages:FlxMapGraphic = new FlxMapGraphic();
+typedef FlxMapGraphic = Map<String, FlxGraphic>;
 
-    // stay for the rest of the games duration, needs restart to clear
-    static var permCachedImages:FlxMapGraphic = new FlxMapGraphic();
+class Paths
+{
+	// will get cleared on state switch
+	static var cachedImages:FlxMapGraphic = new FlxMapGraphic();
 
+	// stay for the rest of the games duration, needs restart to clear
+	static var permCachedImages:FlxMapGraphic = new FlxMapGraphic();
 
+	public function getGraphicFromCache(key:String):FlxGraphic
+	{
+		if (cachedImages.exists(key))
+			return cachedImages.get(key);
+		else if (permCachedImages.exists(key))
+			return permCachedImages.get(key);
+		return null;
+	}
 }
